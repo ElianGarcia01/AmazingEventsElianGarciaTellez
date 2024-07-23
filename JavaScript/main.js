2112342ba36ffbc181b4339544905defaac03595
Elian Garcia <telljua90@gmail.com>
2024-07-23 00:40:16 -0600

    modificacion en mensaje de filtros

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -287,7 +287,7 @@ function pintarTarjetasConFiltro(tarjetasFiltradas) {
       const mensaje = document.createElement('div')
       mensaje.id = 'no-notes'
       mensaje.className = 'text-center'
-      mensaje.innerText = 'UNA DISCULPA, NO SE ENCONTRARON RESULTADOS EN TU BUSQUEDA'
+      mensaje.innerText = 'LO SENTIMOS, NO SE ENCONTRARON RESULTADOS EN TU BUSQUEDA'
       contenedor.appendChild(mensaje)
       return
   }
